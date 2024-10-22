4d3de58045399ec14fbee9d9ebb9476ca32f1bb6
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-10-22 19:04:07 -0300

    sexto commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/dogs/', // Cambia por el nombre de tu repositorio
+  base: '/pi-dogs-web/', // Cambia por el nombre de tu repositorio
   plugins: [react()],
   build: {
     outDir: 'dist',
